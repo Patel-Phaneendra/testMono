28863b34dd7fdd204882dacd041703cf6c7ff5b4
Patel-Phaneendra <99329272+Patel-Phaneendra@users.noreply.github.com>
2022-07-25 14:29:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	if name == "" {
-		name = "from Kotak"
+		name = "from HDFC, Kormangala"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
 }
